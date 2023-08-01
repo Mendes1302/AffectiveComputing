dbb8c4e3fb072ca29690cc87b92997d3fafad19d
Mendes1302 <lmlucasmendes1302@gmail.com>
2023-08-01 11:30:54 -0300

    added code about data_analysis

--- environment.sh
+++ environment.sh
@@ -2,3 +2,6 @@ sudo apt install sqlite3
 sqlite3 songs_database.db
 git config --global core.excludesFile '~/.gitignore'
 
+pip3 install bert-for-tf2
+pip3 install sentencepiece
+pip3 install tensorflow=='2.2.0-rc3'

ea1dd2008dc7b7e7a00d4a3ea68acac7004a59cf
Lucas Mendes Barbosa <lmlucasmendes1302@gmail.com>
2024-01-10 18:00:47 -0300

    Update create_tables.sql

--- data_source/sql/create_tables.sql
+++ data_source/sql/create_tables.sql
@@ -1,22 +1,67 @@
-
-
 CREATE TABLE song(song_id INTEGER PRIMARY KEY NOT NULL, 
                   song_name TEXT NOT NULL, 
                   lyrics TEXT NOT NULL, 
                   vagalume_song_url TEXT NOT NULL, 
-                  vagalume_song_id TEXT NOT NULL);
-
-
+                  vagalume_song_id TEXT NOT NULL)
 
 
 CREATE TABLE artist(artist_id INTEGER PRIMARY KEY NOT NULL, 
                   song_id INTEGER NOT NULL, 
                   artist_full_name TEXT NOT NULL, 
                   vagalume_artist_id TEXT NOT NULL,
-                  FOREIGN KEY(song_id) REFERENCES song(song_id));
+                  FOREIGN KEY(song_id) REFERENCES song(song_id))
 
 
-SELECT * FROM song
-INNER JOIN artist ON song.song_id = artist.song_id;
+CREATE TABLE emotion(emotion_id INTEGER PRIMARY KEY NOT NULL, 
+                  name_emotion TEXT NOT NULL, 
+                  synonym_emotion TEXT NOT NULL)
 
-SELECT * FROM artist;
+
+CREATE TABLE emotional_result (
+    "emotinal_result_id" INTEGER PRIMARY KEY,
+    "song_id" INTEGER,
+    "model_name" TEXT,
+    "admiração" FLOAT,
+    "diversão" FLOAT,
+    "raiva" FLOAT,
+    "aborrecimento" FLOAT,
+    "provação" FLOAT,
+    "confusão" FLOAT,
+    "curiosidade" FLOAT,
+    "desejo" FLOAT,
+    "decepção" FLOAT,
+    "desaprovação" FLOAT,
+    "nojo" FLOAT,
+    "vergonha" FLOAT,
+    "entusiasmo" FLOAT,
+    "medo" FLOAT,
+    "gratidão" FLOAT,
+    "luto" FLOAT,
+    "alegria" FLOAT,
+    "amor" FLOAT,
+    "nervosismo" FLOAT,
+    "otimismo" FLOAT,
+    "orgulho" FLOAT,
+    "alívio" FLOAT,
+    "remorso" FLOAT,
+    "tristeza" FLOAT,
+    "surpresa" FLOAT,
+    "saudade" FLOAT,
+    "inveja" FLOAT,
+    "compaixão" FLOAT,
+    "created_at" DATETIME,
+    "updated_at" DATETIME,
+    "deleted_at" DATETIME,
+    FOREIGN KEY ("song_id") REFERENCES song("song_id"),
+    PRIMARY KEY("emotinal_result_id" AUTOINCREMENT)
+);
+
+
+CREATE TABLE "inputs" (
+	"inputs_id"	INTEGER NOT NULL UNIQUE,
+	"text_name"	TEXT NOT NULL,
+	"source"	TEXT NOT NULL,
+	"emotion_id"	INTEGER NOT NULL,
+	FOREIGN KEY("emotion_id") REFERENCES "emotion"("emotion_id"),
+	PRIMARY KEY("inputs_id" AUTOINCREMENT)
+)
